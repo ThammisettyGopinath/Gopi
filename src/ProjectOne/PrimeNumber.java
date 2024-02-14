ed06a3dfec6843b0d2eab9367bf5f0c97d282627
MY_PC <thammisettygopinath143@gmail.com>
2024-02-14 17:03:21 +0530

    third commit

---
+++ src/ProjectOne/PrimeNumber.java
@@ -0,0 +1,29 @@
+package ProjectOne;
+
+ import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main (String[] args)
+    {
+        System.out.println("Enter Number : ");
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        int count=0;
+        for (int i=1;i<=num;i++)
+        {
+            if(num%i==0)
+            {
+                count++;
+            }
+        }
+        if(count==2)
+        {
+            System.out.println("Prime");
+        }
+        else
+        {
+            System.out.println("Not Prime");
+        }
+        sc.close();
+    }
+}
